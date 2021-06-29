78734818b8c90ab4450a512b25bdff7b57d19fcc
Rai Ricafrente <github@ricafrente.com>
2021-06-29 20:15:59 +0800

    Initial commit of lesson

---
+++ hello.py
@@ -0,0 +1,29 @@
+print("Hello.")
+
+#name = input("What's your name? ")
+#print(f"Hello {name}")
+
+#number_input = int(input("What number "))
+
+#if number_input < 0 :
+    #print(f"{number_input} is negative number")
+#elif number_input > 0 :
+    #print(f"{number_input} is positive number")
+#else :
+    #print(f"{number_input} is zero")
+
+#names = ['Harry', 'Ron', 'Herm']
+
+#names.append("Draco")
+#names.sort()
+#print(names)
+
+#set_name = set()
+
+#set_name.add('A')
+#set_name.add('a')
+#set_name.add(2)
+
+#print(set_name)
+
+#print(f"Number of elements {len(set_name)}")
